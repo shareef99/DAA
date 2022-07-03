a7c30e3dd9410019385a44d3d5318e9c8be001a2
shareef <nadeemshareef934@gmail.com>
2022-07-03 16:49:20 +0530

    Add question details of 8th question

--- 8/Subset.c
+++ 8/Subset.c
@@ -1,4 +1,9 @@
-
+/*
+  Find a subset of a given set S = {s1,s2,.....,sn} of n positive integers whose sum is
+  equal to a given positive integer d. For example, if S=
+  {1, 2, 5, 6, 8} and d = 9 there are two solutions {1,2,6} and {1,8}.A suitable message is to
+  be displayed if the given problem instance doesn't have a solution.
+*/
 
 #include<stdio.h>
 #include<conio.h>
